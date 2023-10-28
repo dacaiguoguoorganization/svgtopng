b424377e396090e4ae5e675928670d2ec3801262
sunyanguo <dacaiguoguo@163.com>
2023-10-28 12:53:43 +0800

    simple

--- svgtopng.py
+++ svgtopng.py
@@ -1,14 +1,4 @@
 from cairosvg import svg2png
-import requests
-
-# 发起GET请求
-url = 'https://img.zdic.net/xz/swdz/9999_7712.svg'
-response = requests.get(url)
-
-# 检查响应状态码
-if response.status_code == 200:
-    svg_code = response.text
-    svg2png(bytestring=svg_code, parent_width=1024, parent_height=1024, write_to='output.png')
-else:
-    print(f'请求失败，状态码: {response.status_code}')
-
+# 设置背景颜色
+background_color = 'white'
+svg2png(url = 'https://img.zdic.net/xz/swdz/9999_7712.svg', background_color=background_color, parent_width=1024, parent_height=1024, write_to='output.png')
